31cfb29d42e3bf293f738adb333d61fcaabe65dd
Alan344 <boris344@yahoo.fr>
2017-11-09 01:16:43 +0100

    Error fixing on user creation script

--- poc-team-cloud-operational/poc-team-cloud-script-ldap/src/main/resources/script/createUser.sh
+++ poc-team-cloud-operational/poc-team-cloud-script-ldap/src/main/resources/script/createUser.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 echo "Enter the default password:"
-read -s pass
+read -s default_pass
 
-while IFS=':' read ip name; do
-        echo "Creating $name account"
-        ssh root@$ip useradd $name -m -p `openssl passwd $pass` -s /bin/bash
-        echo "User $name created"
+echo "Enter the root password of the target VM:"
+read -s pass_root
+
+while IFS='-' read ip name; do
+        nohup sshpass -p $pass_root ssh root@$ip useradd $name -m -p `openssl passwd $pass` & > user_creation.log >> /dev/null
 done < users.txt
